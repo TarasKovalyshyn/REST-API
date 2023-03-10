6f3ca36dee35971ea181149c2a3b7d0f6ed136e1
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-03-11 00:24:53 +0200

    created

--- models/contacts.js
+++ models/contacts.js
@@ -1,9 +1,6 @@
-
-
 const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
-// const { error } = require("console");
 const contactsPath = path.join(__dirname, "./contacts.json");
 
 const listContacts = async () => {
--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://taraskovalyshyn:BC1083KPbc9169ei@cluster0.tkez3bl.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
 mongoose.set("strictQuery", true);
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
 
+mongoose.connect(DB_HOST)
+  .then(() => {
     app.listen(3000);
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
+=======
+const app = require('./app')
+
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000")
+})
+>>>>>>> parent of 86a1841 (created server.js)
