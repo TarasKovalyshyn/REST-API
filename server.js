75e06e5a2c8d102da9036421dc871af32f9e8c7c
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-03-10 22:34:18 +0200

    returned json.lock

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-console.log('create database')
+console.log('create')
     app.listen(3000);
   })
   .catch((error) => {
