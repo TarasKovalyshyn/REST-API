9d62f3ebc77539f7964b0413d01c5e745321075e
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-03-11 03:34:38 +0200

    set up server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const mongoose = require("mongoose");
+const app = require("./app");
 
+const DB_HOST =
+  "mongodb+srv://taraskovalyshyn:BC1083KPbc9169ei@cluster0.tkez3bl.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
-const app = require('./app')
+mongoose.set("strictQuery", true);
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
-
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("success connect");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
