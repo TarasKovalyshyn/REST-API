4b32823cdb658cad7d3b055e4e2288d2c150c01e
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-03-12 16:10:14 +0200

    add nnvironment
    create env, env.example

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-
-const DB_HOST =
-  "mongodb+srv://taraskovalyshyn:BC1083KPbc9169ei@cluster0.tkez3bl.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
-mongoose
-  .connect(DB_HOST)
+mongoose.connect(DB_HOST)
   .then(() => {
     console.log("success connect to server");
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
