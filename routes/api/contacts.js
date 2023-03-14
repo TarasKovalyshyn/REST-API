871c066b5cd088d94a7f176f8e23bb88d617a12a
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-03-14 13:36:39 +0200

    mentor reviev
    created new route (patch)
    creat3ed new schema
    add contacts from dataBase

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const router = express.Router();
 const ctrl = require("../../controllers/contacts");
-const { validateBody } = require("../../middlewares");
-const schemas = require("../../schemas/contacts");
+const { validateBody, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
+const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:id", ctrl.getById);
+router.get("/:id", isValidId, ctrl.getById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
+router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
-router.delete("/:id", ctrl.deleteContact);
+router.delete("/:id", isValidId, ctrl.deleteContact);
+
+router.patch("/:id/favorite",isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite
+);
 
 module.exports = router;
